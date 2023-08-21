69086cf4bec7b2deb9f4c659d136d01bd8a7be69
GuillermoAlexis <guillermo-alexis@hotmail.com>
2023-08-20 21:01:41 -0400

    ADD: bd and dump

---
+++ dump/Postgres/respaldo.sql
@@ -0,0 +1,479 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.4 (Debian 15.4-1.pgdg120+1)
+-- Dumped by pg_dump version 15.4 (Debian 15.4-1.pgdg120+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: app_user; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.app_user (
+    id integer NOT NULL,
+    username character varying(50) NOT NULL,
+    email character varying(100) NOT NULL,
+    password character varying(100) NOT NULL,
+    role_id integer
+);
+
+
+ALTER TABLE public.app_user OWNER TO postgres;
+
+--
+-- Name: app_user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.app_user_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.app_user_id_seq OWNER TO postgres;
+
+--
+-- Name: app_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.app_user_id_seq OWNED BY public.app_user.id;
+
+
+--
+-- Name: feature; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.feature (
+    id integer NOT NULL,
+    name character varying(50) NOT NULL
+);
+
+
+ALTER TABLE public.feature OWNER TO postgres;
+
+--
+-- Name: feature_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.feature_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.feature_id_seq OWNER TO postgres;
+
+--
+-- Name: feature_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.feature_id_seq OWNED BY public.feature.id;
+
+
+--
+-- Name: file; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.file (
+    id integer NOT NULL,
+    filename character varying(255) NOT NULL,
+    status character varying(50) NOT NULL,
+    processed_at timestamp without time zone,
+    user_id integer
+);
+
+
+ALTER TABLE public.file OWNER TO postgres;
+
+--
+-- Name: file_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.file_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.file_id_seq OWNER TO postgres;
+
+--
+-- Name: file_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.file_id_seq OWNED BY public.file.id;
+
+
+--
+-- Name: role; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.role (
+    id integer NOT NULL,
+    name character varying(50) NOT NULL
+);
+
+
+ALTER TABLE public.role OWNER TO postgres;
+
+--
+-- Name: role_feature; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.role_feature (
+    id integer NOT NULL,
+    role_id integer,
+    feature_id integer
+);
+
+
+ALTER TABLE public.role_feature OWNER TO postgres;
+
+--
+-- Name: role_feature_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.role_feature_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.role_feature_id_seq OWNER TO postgres;
+
+--
+-- Name: role_feature_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.role_feature_id_seq OWNED BY public.role_feature.id;
+
+
+--
+-- Name: role_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.role_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.role_id_seq OWNER TO postgres;
+
+--
+-- Name: role_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.role_id_seq OWNED BY public.role.id;
+
+
+--
+-- Name: validation_detail; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.validation_detail (
+    id integer NOT NULL,
+    file_id integer,
+    line_number integer NOT NULL,
+    field_name character varying(50) NOT NULL,
+    error_code integer NOT NULL,
+    error_message character varying(255) NOT NULL
+);
+
+
+ALTER TABLE public.validation_detail OWNER TO postgres;
+
+--
+-- Name: validation_detail_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.validation_detail_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.validation_detail_id_seq OWNER TO postgres;
+
+--
+-- Name: validation_detail_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.validation_detail_id_seq OWNED BY public.validation_detail.id;
+
+
+--
+-- Name: app_user id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.app_user ALTER COLUMN id SET DEFAULT nextval('public.app_user_id_seq'::regclass);
+
+
+--
+-- Name: feature id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.feature ALTER COLUMN id SET DEFAULT nextval('public.feature_id_seq'::regclass);
+
+
+--
+-- Name: file id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.file ALTER COLUMN id SET DEFAULT nextval('public.file_id_seq'::regclass);
+
+
+--
+-- Name: role id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.role ALTER COLUMN id SET DEFAULT nextval('public.role_id_seq'::regclass);
+
+
+--
+-- Name: role_feature id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.role_feature ALTER COLUMN id SET DEFAULT nextval('public.role_feature_id_seq'::regclass);
+
+
+--
+-- Name: validation_detail id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.validation_detail ALTER COLUMN id SET DEFAULT nextval('public.validation_detail_id_seq'::regclass);
+
+
+--
+-- Data for Name: app_user; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.app_user (id, username, email, password, role_id) FROM stdin;
+1	admin	admin@example.com	adminpassword	1
+2	user1	user1@example.com	userpassword	2
+3	user2	user2@example.com	userpassword	2
+\.
+
+
+--
+-- Data for Name: feature; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.feature (id, name) FROM stdin;
+1	Crear Usuario
+2	Editar Usuario
+3	Procesar Archivo
+\.
+
+
+--
+-- Data for Name: file; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.file (id, filename, status, processed_at, user_id) FROM stdin;
+1	20230820_File1.DAT	Procesado	\N	1
+2	20230820_File2.DAT	En Proceso	\N	2
+\.
+
+
+--
+-- Data for Name: role; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.role (id, name) FROM stdin;
+1	Administrador
+2	Usuario
+3	Invitado
+\.
+
+
+--
+-- Data for Name: role_feature; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.role_feature (id, role_id, feature_id) FROM stdin;
+1	1	1
+2	1	2
+3	2	2
+4	2	3
+\.
+
+
+--
+-- Data for Name: validation_detail; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.validation_detail (id, file_id, line_number, field_name, error_code, error_message) FROM stdin;
+1	1	1	Tipo	300	El campo "Tipo de Registro" debe ser numérico.
+2	1	2	Entidad	2	El campo debe ser igual a SERMALUC.
+3	2	1	Tipo	202	Debe venir informado en el archivo un registro tipo 1 (01).
+\.
+
+
+--
+-- Name: app_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.app_user_id_seq', 3, true);
+
+
+--
+-- Name: feature_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.feature_id_seq', 3, true);
+
+
+--
+-- Name: file_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.file_id_seq', 2, true);
+
+
+--
+-- Name: role_feature_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.role_feature_id_seq', 4, true);
+
+
+--
+-- Name: role_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.role_id_seq', 3, true);
+
+
+--
+-- Name: validation_detail_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.validation_detail_id_seq', 3, true);
+
+
+--
+-- Name: app_user app_user_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.app_user
+    ADD CONSTRAINT app_user_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: feature feature_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.feature
+    ADD CONSTRAINT feature_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: file file_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.file
+    ADD CONSTRAINT file_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: role_feature role_feature_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.role_feature
+    ADD CONSTRAINT role_feature_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: role role_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.role
+    ADD CONSTRAINT role_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: validation_detail validation_detail_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.validation_detail
+    ADD CONSTRAINT validation_detail_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: app_user app_user_role_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.app_user
+    ADD CONSTRAINT app_user_role_id_fkey FOREIGN KEY (role_id) REFERENCES public.role(id);
+
+
+--
+-- Name: file file_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.file
+    ADD CONSTRAINT file_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.app_user(id);
+
+
+--
+-- Name: role_feature role_feature_feature_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.role_feature
+    ADD CONSTRAINT role_feature_feature_id_fkey FOREIGN KEY (feature_id) REFERENCES public.feature(id);
+
+
+--
+-- Name: role_feature role_feature_role_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.role_feature
+    ADD CONSTRAINT role_feature_role_id_fkey FOREIGN KEY (role_id) REFERENCES public.role(id);
+
+
+--
+-- Name: validation_detail validation_detail_file_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.validation_detail
+    ADD CONSTRAINT validation_detail_file_id_fkey FOREIGN KEY (file_id) REFERENCES public.file(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
