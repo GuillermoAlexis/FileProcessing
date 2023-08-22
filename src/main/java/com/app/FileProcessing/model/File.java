390d126d2e0ca9cb4379c65e55bc8b6325177699
GuillermoAlexis <guillermo-alexis@hotmail.com>
2023-08-22 00:56:16 -0400

    ADD: file validation entities

---
+++ src/main/java/com/app/FileProcessing/model/File.java
@@ -0,0 +1,38 @@
+package com.app.FileProcessing.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "file")
+@Data
+public class File {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "file_name", nullable = false)
+	private String fileName;
+
+	@Column(name = "status", nullable = false)
+	private String status;
+
+	@Column(name = "processed_at")
+	private Timestamp processedAt;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "user_id")
+	private AppUser user;
+}
