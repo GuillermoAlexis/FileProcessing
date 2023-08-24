225d7374028c07c9d95f4004fb8e7180c07a07f7
GuillermoAlexis <guillermo-alexis@hotmail.com>
2023-08-24 01:22:11 -0400

    ADD: versioning of stable dependencies

--- src/main/java/com/app/FileProcessing/FileProcessingApplication.java
+++ src/main/java/com/app/FileProcessing/FileProcessingApplication.java
@@ -2,8 +2,11 @@ package com.app.FileProcessing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.app.FileProcessing") // Asegura el escaneo de componentes en este paquete y
+//subpaquetes
 public class FileProcessingApplication {
 
 	public static void main(String[] args) {
